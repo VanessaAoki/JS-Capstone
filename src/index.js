9bef5d8cfdf6aef201e8fb8b3f43b2781e9c34d2
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-07 01:33:51 +0100

    Fetch API for movies data

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import './style.css';
+
+const url = "https://api.tvmaze.com/search/shows?q=boys"
+let shows = [];
+const cardWrapper = document.querySelector('#card-wrapper');
+
+// Let know before you start
